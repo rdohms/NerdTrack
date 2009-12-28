3a5870b63fa4a973fad9e36db422317d431cf96e
rdohms <rdohms@93a5328c-a2d4-4e16-a3fa-6929b7865b51>
2009-12-28 00:16:04 +0000

    Skeleton for Institutional pages
    git-svn-id: https://svn.rafaeldohms.com.br/nerdtracker/trunk@23 93a5328c-a2d4-4e16-a3fa-6929b7865b51

--- config/routes.rb
+++ config/routes.rb
@@ -50,4 +50,5 @@ ActionController::Routing::Routes.draw do |map|
   # consider removing or commenting them out if you're using named routes and resources.
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
+  map.connect ':controller/:action'
 end
