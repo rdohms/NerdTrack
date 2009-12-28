87d3526705fc315f2e8478eae5f68e4638f36f0e
rdohms <rdohms@93a5328c-a2d4-4e16-a3fa-6929b7865b51>
2009-12-28 13:45:21 +0000

    Removing trash from config causing crash
    git-svn-id: https://svn.rafaeldohms.com.br/nerdtracker/trunk@30 93a5328c-a2d4-4e16-a3fa-6929b7865b51

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,5 +36,3 @@ CONTATO_EMAIL = ["rafael@rafaeldohms.com.br","listas@rafaeldohms.com.br"]
 config.action_mailer.raise_delivery_errors = true
 config.action_mailer.delivery_method = :smtp
 config.action_mailer.default_content_type = "text/html"
-
-config.contact_emails = ["rafael@rafaeldohms.com.br","listas@rafaeldohms.com.br"]
