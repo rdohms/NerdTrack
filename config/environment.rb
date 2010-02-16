cd5212403e60ce938d58f1e0760d7967239a13a8
dine <dine@93a5328c-a2d4-4e16-a3fa-6929b7865b51>
2010-02-16 14:28:14 +0000

    ajuste css verifica usuario
    git-svn-id: https://svn.rafaeldohms.com.br/nerdtracker/trunk@113 93a5328c-a2d4-4e16-a3fa-6929b7865b51

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.4' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 
 #Version Info
 NERDTRACK_VERSION = "Beta 0.7"
