d4bebb33d6118faffc78a6a2658761422ba69a2e
rdohms <rdohms@93a5328c-a2d4-4e16-a3fa-6929b7865b51>
2010-04-05 13:52:57 +0000

    Moderation Request validation and edit/del links visible to admins
    git-svn-id: https://svn.rafaeldohms.com.br/nerdtracker/branches/beta-08@154 93a5328c-a2d4-4e16-a3fa-6929b7865b51

--- app/helpers/mng_helper.rb
+++ app/helpers/mng_helper.rb
@@ -1,25 +1,25 @@
 module MngHelper
   
   def show_edit_track_link(track)
-    if !current_user.nil? && track.user.id == current_user.id
+    if !current_user.nil? && (track.user.id == current_user.id || current_user.admin?)
       link_to image_tag("icons/pencil.png", :alt => "Alterar Música"), edit_track_path(track)
     end
   end
   
   def show_edit_quote_link(quote)
-    if !current_user.nil? && quote.user.id == current_user.id
+    if !current_user.nil? && (quote.user.id == current_user.id || current_user.admin?)
       link_to image_tag("icons/pencil.png", :alt => "Alterar Frase"), edit_quote_path(quote)
     end
   end
   
   def show_del_track_link(track)
-    if !current_user.nil? && track.user.id == current_user.id
+    if !current_user.nil? && (track.user.id == current_user.id || current_user.admin?)
       link_to image_tag("icons/delete.png", :alt => "Remover Música"), track, :confirm => 'Você tem certeza?', :method => :delete 
     end
   end
   
   def show_del_quote_link(quote)
-    if !current_user.nil? && quote.user.id == current_user.id
+    if !current_user.nil? && (quote.user.id == current_user.id || current_user.admin?)
       link_to image_tag("icons/delete.png", :alt => "Remover Frase"), quote, :confirm => 'Você tem certeza?', :method => :delete 
     end
   end
--- app/models/moderation_request.rb
+++ app/models/moderation_request.rb
@@ -1,5 +1,7 @@
 class ModerationRequest < ActiveRecord::Base
   
+  validates_presence_of :target_id, :target_type, :user, :message => "não pode ficar em branco."
+  
   belongs_to :user
   
 end
