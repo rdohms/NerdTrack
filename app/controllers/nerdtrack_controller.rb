daa70ff452bbeb1ab368c040f1bb2bc3050b9302
rdohms <rdohms@93a5328c-a2d4-4e16-a3fa-6929b7865b51>
2010-07-16 02:08:32 +0000

    Adding SPAM checking for contact form in a very simplified way
    git-svn-id: https://svn.rafaeldohms.com.br/nerdtracker/branches/beta-08@159 93a5328c-a2d4-4e16-a3fa-6929b7865b51

--- app/controllers/nerdtrack_controller.rb
+++ app/controllers/nerdtrack_controller.rb
@@ -18,13 +18,26 @@ class NerdtrackController < ApplicationController
   end
 
   def sendmail
-    if Notifications.deliver_contact(params[:contato])
-       flash[:notice] = "Email enviado com sucesso!"
-       redirect_to(:back)
-     else
-       flash.now[:error] = "Erro ao enviar email!"
-       render :contato
-     end
+    
+    logger.info params[:contato][:captcha]
+    
+    # SPAM Check
+    if params[:contato][:captcha] != "8"
+      
+      flash[:notice] = "Você falhou ao passar pelo sistema anti-spam"
+      render :contato
+    
+    else
+    
+      if Notifications.deliver_contact(params[:contato])
+         flash[:notice] = "Email enviado com sucesso!"
+         redirect_to(:contato)
+       else
+         flash.now[:error] = "Erro ao enviar email!"
+         render :contato
+       end
+       
+    end
 
   end
 end
